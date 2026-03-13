106ce4b14820a340382a741f36bb94b085a99b06
TrungLH <trungluuhoang456@gmail.com>
2026-03-13 07:48:57 +0700

    Add ConversationRepository

--- src/main/java/com/trung/chat/chatapp/repository/ConversationRepository.java
+++ src/main/java/com/trung/chat/chatapp/repository/ConversationRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ConversationRepository extends JpaRepository<Conversation, UUID> {
+
+    Conversation findPrivateConversation(UUID userA, UUID userB);
 }
