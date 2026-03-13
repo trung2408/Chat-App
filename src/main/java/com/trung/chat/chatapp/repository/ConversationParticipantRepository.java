f678ce9ad51e0607a775630190503cbb837ef3b7
TrungLH <trungluuhoang456@gmail.com>
2026-03-13 07:49:24 +0700

    Add ConversationParticipantRepository

--- src/main/java/com/trung/chat/chatapp/repository/ConversationParticipantRepository.java
+++ src/main/java/com/trung/chat/chatapp/repository/ConversationParticipantRepository.java
@@ -1,4 +1,9 @@
 package com.trung.chat.chatapp.repository;
 
-public class ConversationParticipantRepository {
+import com.trung.chat.chatapp.entity.ConversationParticipant;
+import com.trung.chat.chatapp.entity.ConversationParticipantId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConversationParticipantRepository extends JpaRepository<ConversationParticipant, ConversationParticipantId> {
+    int countByUser(ConversationParticipant conversationParticipant);
 }
